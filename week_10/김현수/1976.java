25220fae785cecc6ebe5d4a5b25ccf940f9e1818
hsmygit <myhskim79@gmail.com>
2025-03-17 21:10:33 +0900

    10week

---
+++ week_10/김현수/1976.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringJoiner;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+/*
+* 여행 가자
+* N개의 도시, M개의 여행 계획에 속한 도시들의 수
+* 여행 계획에 속한 도시들이 모두 연결되어 있는지 확인
+* 각 도시들의 대표 도시를 찾기
+*
+ */
+public class Main {
+
+    static String SUCCESS = "YES";
+    static String FAIL = "NO";
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        int M = Integer.parseInt(bf.readLine());
+
+        int[][] cities = new int[N + 1][N + 1];
+        int[] route = new int[M];
+
+        //도시간 연결 정보 입력
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            for (int j = 1; j <= N; j++) {
+                cities[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        //여행 경로 입력
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < M; i++) {
+            route[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(N, M, cities, route));
+    }
+    static String solution(int N, int M, int[][] cities, int[] route) {
+        int[] group = IntStream.range(0, N + 1).toArray();
+
+        // 도시간 연결 처리
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (cities[i][j] == 1) {
+                    int a = find(i, group);
+                    int b = find(j, group);
+                    group[Math.max(a, b)] = Math.min(a, b);
+                }
+            }
+        }
+
+        // 여행 경로 확인
+        for (int i = 1; i < M; i++) {
+            if (find(route[0], group) != find(route[i], group)) {
+                return FAIL;
+            }
+        }
+        return SUCCESS;
+    }
+
+    static int find(int n, int[] group) {
+        if (n != group[n]) group[n] = find(group[n], group);
+        return group[n];
+    }
+}
---
+++ week_10/김현수/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+/*
+* 사이클 게임
+* N개의 점, M개의 선
+* 두 점을 선택해 연결
+* 사이클이 생기면 종료
+*
+* 유니온 파인드로 사이클 확인
+* - 연결하려는 두 점이 이미 같은 집합에 속해있으면 사이클이 생김
+* */
+public class Main {
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] lines = new int[M][2];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            lines[i][0] = Integer.parseInt(st.nextToken());
+            lines[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(N, M, lines));
+    }
+
+    static int solution(int N, int M, int[][] lines){
+        int[] group = IntStream.range(0, N).toArray();
+
+        for (int i = 0; i < M; i++) {
+            int a = find(lines[i][0], group);
+            int b = find(lines[i][1], group);
+
+            if(a == b) return i + 1; //사이클이 만들어짐
+
+            group[Math.max(a, b)] = Math.min(a, b);
+        }
+        return 0;
+    }
+
+    static int find(int n, int[] group) {
+        if (n != group[n]) group[n] = find(group[n], group);
+        return group[n];
+    }
+}
