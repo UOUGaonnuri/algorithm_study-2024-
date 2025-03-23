eef0c50fc471e477cca977ac279d1129bd82ca85
cba0898-k <cba0898@gmail.com>
2025-03-23 21:46:35 +0900

    Week10 폴더 정리

--- week_10/박서준/Project4/소스.cpp
+++ week_10/박서준/Project4/소스.cpp
@@ -1,32 +1,81 @@
 ﻿#include <iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
 using namespace std;
+struct WaterInfo
+{
+	int a;
+	int b;	
+	int v;
+};
 
-int findRoot(int idx, vector<int>& unions)
+int findRoot(vector<int>& unions, int idx)
 {
 	if (unions[idx] == idx)
 		return idx;
-	return unions[idx] = findRoot(unions[idx], unions);
+	return unions[idx] = findRoot(unions, unions[idx]);
 }
 
-void unionFind(int i, int j, vector<int>& unions)
+int solution(int N, vector<int>& digCosts, vector<vector<int>>& unionCosts)
 {
-	int iRoot = findRoot(i, unions);
-	int jRoot = findRoot(j, unions);
-
-	if (iRoot != jRoot)
+	int answer = 0;
+	vector<int> unions(N + 1);
+	for (int i = 1;i <= N;i++)
 	{
-		// 값이 낮은 루트로 선택
-		if (iRoot < jRoot)
+		unions[i] = i;
+	}
+	vector<WaterInfo> waterInfos;
+	for (int i = 1;i <= N;i++)
+	{
+		for (int j = i + 1;j <= N;j++)
 		{
-			unions[jRoot] = iRoot;
-		}
-		else
-		{
-			unions[iRoot] = jRoot;
+			WaterInfo waterInfo;
+			waterInfo.a = i;
+			waterInfo.b = j;
+			waterInfo.v = unionCosts[i][j];
+			waterInfos.push_back(waterInfo);
 		}
 	}
+
+	sort(waterInfos.begin(), waterInfos.end(), 
+		[](const WaterInfo& a, const WaterInfo& b)
+		{
+			return a.v < b.v;
+		});
+
+	for (const WaterInfo& waterInfo : waterInfos)
+	{
+		int aRoot = findRoot(unions, waterInfo.a);
+		int bRoot = findRoot(unions, waterInfo.b);
+
+		//사이클이면 스킵
+		if (aRoot == bRoot) continue;
+
+		// 연결이 더 싸면
+		int cheaperRoot = digCosts[aRoot] < digCosts[bRoot] ? aRoot : bRoot;
+		int shitRoot = digCosts[aRoot] < digCosts[bRoot] ? bRoot : aRoot;
+		if(waterInfo.v  < digCosts[shitRoot])
+		{
+			// 루트를 땅 파기 싼 친구로 설정
+			unions[shitRoot] = cheaperRoot;
+
+			answer += waterInfo.v;
+		}
+	}
+
+	// 땅 파기
+	for (int i = 1;i <= N;i++)
+	{
+		// 루트들만
+		int unionRoot = findRoot(unions, i);
+		if (i == unionRoot)
+		{
+			answer += digCosts[unionRoot];
+		}
+	}
+
+	return answer;
 }
 
 int main()
@@ -36,25 +85,20 @@ int main()
 
 	int N;
 	cin >> N;
-	vector<int> unions(N + 1);
+	vector<int> digCosts(N + 1);
+	vector<vector<int>> unionCosts(N + 1, vector<int>(N + 1));
 	for (int i = 1;i <= N;i++)
 	{
-		unions[i] = i;
+		cin >> digCosts[i];
 	}
-	int result = -1;
 	for (int i = 1;i <= N;i++)
 	{
-		string input;
-		cin >> input;
-		for (int j = i;j <= N;j++)
+		for (int j = 1;j <= N;j++)
 		{
-			if (input[j - 1] == 'Y')
-			{
-				unionFind(i, j, unions);
-			}
+			cin >> unionCosts[i][j];
 		}
 	}
 
-	cout << result;
+	cout << solution(N, digCosts, unionCosts);
 	return 0;
 }
